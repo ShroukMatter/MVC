f82126803b00d3e0bedc4b5dad7e76edf8f2af64
Shrouk Matar <shroukk.matar@gmail.com>
2024-07-29 01:20:31 +0300

    Add project files.

---
+++ MVC/Models/Property.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using static System.Net.Mime.MediaTypeNames;
+using System;
+
+namespace MVC.Models
+{
+    public class Property
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Type { get; set; } //House, Apartment, Commercial
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Address { get; set; }
+
+        public int Bedrooms { get; set; }
+        public int Bathrooms { get; set; }
+        public int SquareFeet { get; set; }
+
+        public string Status { get; set; } //Available, Sold, Pending
+
+        public Decimal Price { get; set; }
+
+    }
+}
+
